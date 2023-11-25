29d452e119ce9a7768aa16f134da9a272d5a2608
pavvloff <pavvloff@gmail.com>
2023-11-25 14:38:51 +0100

    Update semantic_search.py

--- gradio_app/backend/semantic_search.py
+++ gradio_app/backend/semantic_search.py
@@ -10,10 +10,19 @@ DB_TABLE_NAME = ""
 # Setting up the logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Enable multiple retrievers
 retrievers = {}
-retrievers['MiniLM'] = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-retrievers['GteLarge'] = SentenceTransformer('thenlper/gte-large')
-retrievers['OpenAI'] = None
+
+def openai_embedding(text):
+  rs = openai.Embedding.create(input=[text], engine="text-embedding-ada-002")
+  return rs["data"][0]["embedding"]
+
+minilm = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+gtelarge = SentenceTransformer('thenlper/gte-large')
+retrievers['MiniLM'] = lambda t: minilm.encode(t)
+retrievers['GteLarge'] = lambda t: gtelarge.encode(t)
+retrievers['OpenAI'] = openai_embedding
 
 # db
 db_uri = os.path.join(Path(__file__).parents[1], ".lancedb")
